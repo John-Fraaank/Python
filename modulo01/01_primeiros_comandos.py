860a2450d5a2a53944e1663f9ddd300a93d9b1e6
John Frank <139596702+John-Fraaank@users.noreply.github.com>
2026-06-08 20:04:38 -0300

    meus primeiros códigos em Python
    primeiros códigos com desafios

---
+++ modulo01/01_primeiros_comandos.py
@@ -0,0 +1,12 @@
+
+
+print("Hello, World!")
+"""variáveis são usadas para armazenar informações que podem ser usadas posteriormente no programa."""
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+"""O comando print() é usado para exibir informações na tela. Ele pode ser usado para mostrar o valor de variáveis, mensagens ou qualquer outra informação que você queira compartilhar com o usuário."""
+print("Seu nome é", nome, "e eu tenho", idade, "anos.")
+print("Seu peso é", peso, "kg.")
+
---
+++ modulo01/03_desafio.py
@@ -0,0 +1,5 @@
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+"""Para realizar operações matemáticas, é necessário converter as entradas do usuário para o tipo numérico adequado. No caso de números inteiros, usamos a função int()."""
+soma = int(numero1) + int(numero2)
+print("A soma dos números é:", soma)
